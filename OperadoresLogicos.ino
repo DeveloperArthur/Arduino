9b2468e9da35274a4835806c34ecfd1d6cdc1c25
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2019-04-27 13:49:44 -0300

    Add files via upload

---
+++ OperadoresLogicos.ino
@@ -0,0 +1,139 @@
+
+int valor1 = 120;
+int valor2 = 148;
+int valor3 = 120;
+
+void setup() {
+  Serial.begin(9600);  
+
+  Serial.print("true = ");
+  Serial.println(true);
+
+  Serial.print("false = ");
+  Serial.println(false);
+
+  Serial.print("HIGH = ");
+  Serial.println(HIGH);
+
+  Serial.print("LOW = ");
+  Serial.println(LOW);
+  
+  
+  Serial.println();
+  Serial.print("valor1 = ");
+  Serial.println(valor1);
+
+  Serial.print("valor2 = ");
+  Serial.println(valor2);
+
+  Serial.print("valor3 = ");
+  Serial.println(valor3);
+
+  Serial.println();
+  Serial.println("****** OPERADORES DE COMPARACAO ******");
+  Serial.print("valor1 == valor2 ? ---> ");
+  Serial.println(valor1 == valor2);
+
+  Serial.print("valor1 == valor3 ? ---> ");
+  Serial.println(valor1 == valor3);
+
+  Serial.println();
+  Serial.print("valor1 != valor2 ? ---> ");
+  Serial.println(valor1 != valor2);
+
+  Serial.print("valor1 != valor3 ? ---> ");
+  Serial.println(valor1 != valor3);
+
+  Serial.println();
+  Serial.print("valor2 > valor3 ? ---> ");
+  Serial.println(valor2 > valor3);
+
+  Serial.print("valor1 > valor3 ? ---> ");
+  Serial.println(valor1 > valor3);
+
+  Serial.println();
+  Serial.print("valor2 >= valor3 ? ---> ");
+  Serial.println(valor2 >= valor3);
+
+  Serial.print("valor1 >= valor3 ? ---> ");
+  Serial.println(valor1 >= valor3);
+
+
+  Serial.println();
+  Serial.println("****** OPERADORES LOGICOS ******");
+
+  Serial.println();
+  Serial.println("------ OPERADOR NAO (NOT)");
+  Serial.print(" !true ? ---> ");
+  Serial.println(!true);
+
+  Serial.print("!false ? ---> ");
+  Serial.println(!false);
+
+  Serial.println();
+  Serial.print(" !(valor1 == valor3) ? ---> ");
+  Serial.println(!(valor1 == valor3));
+
+  Serial.print("!(valor1 == valor2) ? ---> ");
+  Serial.println(!(valor1 == valor2));
+
+  
+  Serial.println();
+  Serial.println("------ OPERADOR OU (OR)");
+  Serial.print("true ou true ? ---> ");
+  Serial.println(true || true);
+
+  Serial.print("true ou false ? ---> ");
+  Serial.println(true || false);
+
+  Serial.print("false ou true ? ---> ");
+  Serial.println(false || true);
+
+  Serial.print("false ou false ? ---> ");
+  Serial.println(false || false);
+
+  Serial.println();
+  Serial.print("(valor1 == valor3)  ||  (valor2 > valor3) ? ---> ");
+  Serial.println( (valor1 == valor3) || (valor2 > valor3) );
+
+  Serial.print("(valor2 >= valor3)  ||  (valor1 == valor2) ? ---> ");
+  Serial.println( (valor2 >= valor3) || (valor1 == valor2) );
+
+  Serial.print("(valor1 != valor3)  ||  (valor2 > valor3) ? ---> ");
+  Serial.println((valor1 != valor3) || (valor2 > valor3));
+
+  Serial.print("(valor1 == valor2)  ||  (valor1 != valor3) ? ---> ");
+  Serial.println((valor1 == valor2) || (valor1 != valor3));
+
+  Serial.println();
+  Serial.println("------ OPERADOR E (AND)");
+  Serial.print("true e true ? ---> ");
+  Serial.println(true && true);
+
+  Serial.print("true e false ? ---> ");
+  Serial.println(true && false);
+
+  Serial.print("false e true ? ---> ");
+  Serial.println(false && true);
+
+  Serial.print("false e false ? ---> ");
+  Serial.println(false && false);
+
+  Serial.println();
+  Serial.print("(valor1 == valor3)  &&  (valor2 > valor3) ? ---> ");
+  Serial.println( (valor1 == valor3) && (valor2 > valor3) );
+
+  Serial.print("(valor2 >= valor3)  &&  (valor1 == valor2) ? ---> ");
+  Serial.println( (valor2 >= valor3) && (valor1 == valor2) );
+
+  Serial.print("(valor1 != valor3)  &&  (valor2 > valor3) ? ---> ");
+  Serial.println((valor1 != valor3) && (valor2 > valor3));
+
+  Serial.print("(valor1 == valor2)  &&  (valor1 != valor3) ? ---> ");
+  Serial.println((valor1 == valor2) && (valor1 != valor3));
+
+}
+
+void loop() {
+
+}
---
+++ String2.ino
@@ -0,0 +1,60 @@
+String textoRecebido = "";
+unsigned long delay1 = 0;
+
+String padR( String texto, int tamanho, char caractere = ' ' );
+String padL( String texto, int tamanho, char caractere = ' ' );
+String strZero( String texto, int tamanho );
+String replicate( char caractere, int vezes );
+
+void setup() {  
+  Serial.begin(9600);  
+ 
+}
+
+void loop() {
+  char caracter;
+  
+  if (Serial.available()) {
+     caracter = Serial.read();
+     textoRecebido += caracter;
+     delay1 = millis();
+  }
+
+  if (((millis() - delay1) > 10) && (textoRecebido != "")) {
+     Serial.print("Texto digitado: ");
+     Serial.println(textoRecebido);
+
+     textoRecebido = "";
+  }
+}
+
+String padR( String texto, int tamanho, char caractere) {
+  if (texto.length() > tamanho) {
+     return texto.substring( texto.length() - tamanho, texto.length() );  
+  } else {
+     return replicate( caractere, tamanho - texto.length() ) + texto;  
+  }
+}
+
+String padL( String texto, int tamanho, char caractere) {
+  if (texto.length() > tamanho) {
+     texto.remove( tamanho ); 
+     return texto; 
+  } else {
+     return texto + replicate( caractere, tamanho - texto.length() );  
+  }
+}
+
+String strZero( String texto, int tamanho ) {
+  return padR( texto, tamanho, '0');
+}
+
+String replicate( char caractere, int vezes ) {
+  String temp;
+
+  for (int nL=0; nL < vezes; nL++) {
+      temp += caractere;   
+  }
+  return temp;
+}
+
